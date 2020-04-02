5751e555faf18a76147de4e9b76ee12e6d69d5a8
Michael Stallings <stall84@gmail.com>
2020-04-02 09:48:47 -0400

    finished moving in the ipGeo API

--- script.js
+++ script.js
@@ -2,10 +2,24 @@
 
 // Section to hold the API communication mechansim .
 
-const url1 = "https://api.worldweatheronline.com/premium/v1/weather.ashx";
-const apiKey = "0eab6e3837ad474491b152802202103";
+const ipGeoURL = 'https://api.ipgeolocation.io/ipgeo';
+const ipGeoKEY = '?apiKey=7fe2f42dd89d4dc6b2f11b4b47167af0';
+const ipGeoCallURL = `${ipGeoURL}${ipGeoKEY}&fields=geo`;
+
+fetch(ipGeoCallURL)
+    .then(res => res.json())
+    .then(data => console.log('ipGeo Data: ', data));
+
+
+
+const wwXURL = "https://api.worldweatheronline.com/premium/v1/weather.ashx";
+const wwXKey = "0eab6e3837ad474491b152802202103";
 // hardcoding atlanta's lat/long initially
 var latLon = `&q=33.75,84.39`
-const callURL = `${url1}?key=${apiKey}${latLon}&format=json`;
+const wwXcallURL = `${wwXURL}?key=${wwXKey}${latLon}&format=json`;
 
-console.log(callURL);
+//console.log(callURL);
+
+fetch(wwXcallURL)
+    .then(res => res.json())
+    .then(data => console.log('wwX Data: ', data));
